fc35856b3e58c1939321166f0824bc1f899c9783
Naashw <hasan.taskin.pro@gmail.com>
2024-01-07 21:04:58 +0100

    remove unused import

--- src/context/virtualTour/adapters/secondary/database/VirtualTourRepository.adapter.ts
+++ src/context/virtualTour/adapters/secondary/database/VirtualTourRepository.adapter.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Prisma, VirtualTourPosition } from '@prisma/client';
+import { Prisma } from '@prisma/client';
 import {
 	VirtualTourDto,
 	VirtualTourRepositoryPort,
