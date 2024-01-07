eeb32e31a5aa30240b4903184a4a3c96f8c0c2d9
Naashw <hasan.taskin.pro@gmail.com>
2024-01-07 21:04:19 +0100

    feat: can up and down docker containers with dc sh

--- dc.sh
+++ dc.sh
@@ -12,14 +12,27 @@ if ! command -v docker-compose &> /dev/null; then
     exit 1
 fi
 
+# Vérifie les arguments du script
+if [ "$#" -eq 0 ]; then
+    echo "Utilisation : $0 [up|down]"
+    exit 1
+fi
+
 # Changez le répertoire de travail vers celui contenant docker-compose.yml
 cd .
 
-# Exécutez Docker Compose pour lancer les conteneurs
-docker-compose up -d
-
-# Affichez un message de confirmation
-echo "Les conteneurs Docker Compose ont été lancés avec succès."
+if [ "$1" == "up" ]; then
+    # Exécutez Docker Compose pour lancer les conteneurs
+    docker-compose up -d
+    echo "Les conteneurs Docker Compose ont été lancés avec succès."
+elif [ "$1" == "down" ]; then
+    # Arrêtez les conteneurs Docker Compose
+    docker-compose down
+    echo "Les conteneurs Docker Compose ont été arrêtés avec succès."
+else
+    echo "Option non reconnue : $1. Utilisation : $0 [up|down]"
+    exit 1
+fi
 
 # Facultatif : Vous pouvez ajouter d'autres commandes ou personnalisation ici si nécessaire.
 
