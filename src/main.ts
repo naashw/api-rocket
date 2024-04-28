961e217fddad7705e622738b71403087be12795b
Naashw <hasan.taskin.pro@gmail.com>
2024-04-28 21:02:33 +0200

    feat(static): add static folder

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function Bootstrap() {
-	const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create<NestExpressApplication>(AppModule);
 	app.enableCors({ origin: '*' });
+	console.log(process.env.STATIC_IMAGES_PATH);
+	app.useStaticAssets(process.env.STATIC_IMAGES_PATH, {
+		prefix: '/static/',
+	});
 	await app.listen(3001);
 }
 Bootstrap();
